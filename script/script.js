9a221bc8eaa6c80f4afe27a61ba4f3d9f14d1e17
marcos ventura <marcosventura.dev@gmail.com>
2023-01-08 02:13:29 -0300

    resolvendo o problema de erro de atribuição de ação antes dos números, agora as ações permitidas é o sinal de + e de -

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 
-function calcular( tipo, valor){
+function calcular(tipo, valor){
 
     const regex = new RegExp(/[\+\-\*\/\.]$/);
     const resultado = document.getElementById('resultado')
@@ -9,15 +9,24 @@ function calcular( tipo, valor){
     const acaoDigitado = regex.test(val)
     const acao = tipo === 'acao'
     const valores = tipo === 'valor'
+    const resulteRegex = !acaoRealizada || !acaoDigitado
+   
     
     if(acao){
         if(valor === 'c'){
             resultado.value = "";
         }
-       if(!acaoRealizada || !acaoDigitado){ 
-         if(valor === val[i]){
-            resultado.value += valor;
-         }
+       if(resultado.value == ""){
+        val[3]= val.indexOf("") 
+        val[2]=val.indexOf("") 
+        
+          }
+       if(resulteRegex){ 
+      
+            if(valor === val[i]){
+                        resultado.value += valor;
+                    }
+
         }
         if(valor === '='){
            let campo = eval(resultado.value);
