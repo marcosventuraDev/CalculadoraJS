062c6b93a6a81bdedd3788366bc2e28e68ac0b57
marcos ventura <marcosventura.dev@gmail.com>
2023-01-07 21:00:43 -0300

    implementando a lógica do sistema

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
 function calcular( tipo, valor){
-    console.log(tipo, valor)
+    if(tipo === 'acao'){
+        if(valor === 'c'){
+            document.getElementById('resultado').value = "";
+        }
+         if(valor === '+'  || valor === '-'  || valor === '*'  || valor ==='/'  || valor === '.'){
+            document.getElementById('resultado').value += valor;
+        }
+        if(valor == '='){
+           let campo = eval(document.getElementById('resultado').value);
+           document.getElementById('resultado').value = campo;
+        }
+
+    }else if(tipo == 'valor'){
+        /*let valor_campo = document.getElementById('resultado').value;*/
+        document.getElementById('resultado').value += valor;
+    }
 }
